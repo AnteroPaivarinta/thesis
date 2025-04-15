b00591c0510f141f4d8332767545e65b6a4aa385
AnteroPaivarinta <antsapaivarinta@gmail.com>
2025-04-15 20:08:38 +0300

    new test  conf

--- babel.config.mjs
+++
@@ -1,8 +0,0 @@
-// babel.config.js (ESM)
-export default {
-  presets: [
-    '@babel/preset-env',           // ES6+ JavaScriptin tuki
-    '@babel/preset-react',         // JSX:n tuki
-    '@babel/preset-typescript',    // TypeScriptin tuki
-  ],
-};
